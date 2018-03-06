65207f2f740340850d9725e5a5f3e85281b81e8b
Lucas Alves <lucasalves.s@outlook.com>
2018-03-06 13:41:36 -0300

    Remove class Client from admin order

--- order/admin.py
+++ order/admin.py
@@ -2,10 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Order, Client
+from .models import Order
 
 # Register your models here.
 
 
 admin.site.register(Order)
-admin.site.register(Client)
