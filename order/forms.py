0bba9f82b84018dc931919751ae1baba765b88de
Lucas Alves <lucasalves.s@outlook.com>
2018-03-20 14:13:58 -0300

    Form template order

--- order/forms.py
+++ order/forms.py
@@ -10,3 +10,10 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['product', 'client', 'quantity', 'description', 'sale_price','delivered', 'canceled']
+    def __init__(self,*args,**kwargs):
+        super(OrderForm, self).__init__(*args, **kwargs)
+        self.fields['product'].widget.attrs['style'] = 'width:65%'
+        self.fields['client'].widget.attrs['style'] = 'width:65%'
+        self.fields['quantity'].widget.attrs['style'] = 'width:20%'
+        self.fields['sale_price'].widget.attrs['style'] = 'width:30%'
+        self.fields['client'].widget.attrs['style'] = 'width:65%'
