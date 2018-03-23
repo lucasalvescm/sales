6133f4fc73f380389024147b3b7cd84527586795
Lucas Alves <lucasalves.s@outlook.com>
2018-03-23 10:50:21 -0300

    Datetimepicer to order date

--- order/forms.py
+++ order/forms.py
@@ -18,16 +18,24 @@ class OrderForm(forms.ModelForm):
         model = Order
         fields = [
             'product', 'client', 'quantity', 'description',
-            'sale_price', 'delivered', 'canceled', 'delivered_date'
+            'sale_price', 'delivered', 'canceled',
+            'delivered_date', 'order_date'
         ]
         widgets = {
-            'delivered_date': forms.DateInput(attrs={'class': 'datepicker', 'id': 'data_input'})
+            'delivered_date': forms.DateInput(
+                attrs={'class': 'datepicker', 'id': 'data_input'}
+            ),
+            'order_date': forms.DateInput(
+                attrs={'class': 'datepicker', 'id': 'data_input'}
+            )
         }
-    def __init__(self,*args,**kwargs):
+
+    def __init__(self, *args, **kwargs):
         super(OrderForm, self).__init__(*args, **kwargs)
         self.fields['product'].widget.attrs['style'] = 'width:65%'
         self.fields['client'].widget.attrs['style'] = 'width:65%'
         self.fields['quantity'].widget.attrs['style'] = 'width:20%'
-        self.fields['delivered_date'].widget.attrs['style'] = 'width:20%'
+        self.fields['delivered_date'].widget.attrs['style'] = 'width:30%'
+        self.fields['order_date'].widget.attrs['style'] = 'width:30%'
         self.fields['sale_price'].widget.attrs['style'] = 'width:30%'
         self.fields['description'].widget.attrs['style'] = 'width:65%;height:20%'
