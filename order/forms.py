f3aaac40b25831633fb8ef431d12ec032b8c08d8
Lucas Alves <lucasalves.s@outlook.com>
2018-03-23 10:05:40 -0300

    Datetime picker

--- order/forms.py
+++ order/forms.py
@@ -5,15 +5,29 @@ from .models import Order
 
 
 class OrderForm(forms.ModelForm):
-    delivered = forms.ChoiceField(label='Entregue?',required=False, widget=forms.CheckboxInput, choices=((True, 'Sim'), (False, 'Não')))
-    canceled = forms.ChoiceField(label='Cancelado?',required=False, widget=forms.CheckboxInput, choices=((True, 'Sim'), (False, 'Não')))
+    delivered = forms.ChoiceField(
+        label='Entregue?', required=False, widget=forms.CheckboxInput,
+        choices=((True, 'Sim'), (False, 'Não'))
+    )
+    canceled = forms.ChoiceField(
+        label='Cancelado?', required=False, widget=forms.CheckboxInput,
+        choices=((True, 'Sim'), (False, 'Não'))
+    )
+
     class Meta:
         model = Order
-        fields = ['product', 'client', 'quantity', 'description', 'sale_price','delivered', 'canceled']
+        fields = [
+            'product', 'client', 'quantity', 'description',
+            'sale_price', 'delivered', 'canceled', 'delivered_date'
+        ]
+        widgets = {
+            'delivered_date': forms.DateInput(attrs={'class': 'datepicker', 'id': 'data_input'})
+        }
     def __init__(self,*args,**kwargs):
         super(OrderForm, self).__init__(*args, **kwargs)
         self.fields['product'].widget.attrs['style'] = 'width:65%'
         self.fields['client'].widget.attrs['style'] = 'width:65%'
         self.fields['quantity'].widget.attrs['style'] = 'width:20%'
+        self.fields['delivered_date'].widget.attrs['style'] = 'width:20%'
         self.fields['sale_price'].widget.attrs['style'] = 'width:30%'
         self.fields['description'].widget.attrs['style'] = 'width:65%;height:20%'
